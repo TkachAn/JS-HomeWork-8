8604d940df68cc354f2c473ccf129162f8c3c258
TkachAn <4ndrey.tkachenko@gmail.com>
2022-06-11 22:56:51 +0300

    vimeo

---
+++ src/js/02-video.js
@@ -0,0 +1,11 @@
+import { throttle } from 'lodash';
+import Player from '@vimeo/player';
+
+const LOCAL_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(({seconds}) => {
+	localStorage.setItem(LOCAL_KEY, seconds);},1000));
+
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY));
