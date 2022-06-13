e77f1e9d66211dc27b42c904dc3b44020142c873
TkachAn <4ndrey.tkachenko@gmail.com>
2022-06-13 15:06:03 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,49 @@
 import { throttle } from 'lodash';
+
+const FORM = 0;
 const LOCAL_KEY = "feedback-form-state";
-const fomrState = document.querySelector(".feedback-form");
-const fomrInput = document.querySelector(".feedback-form input");
-const fomrText = document.querySelector(".feedback-form textarea"); 
-;
-const mem = {};
-
-fomrState.addEventListener('submit', onFormSub);
-fomrState.addEventListener('input', onText);//throttle(onText,1000);
-// fomrState.addEventListener('storage', setText);
-setText();
+const form = document.forms[FORM]; 
 
 
+form.addEventListener('submit', onFormSub);
+form.addEventListener('input', throttle(onData,500));
+
+if(localStorage.getItem(LOCAL_KEY)){setText();}
+
+//--*function*--//
 function onFormSub(event){
 	event.preventDefault();
-	
+	//--*--//
 	locStor = localStorage.getItem(LOCAL_KEY);
-	console.log('do',locStor);
- 	console.log('do',JSON.parse(locStor));
+	console.log('JSON',locStor);
+ 	console.log('OBJ',JSON.parse(locStor));
 	localStorage.removeItem(LOCAL_KEY);
-	fomrInput.value = '';
-	fomrText.value = '';
-	mem.email = '';
-	mem.message = '';
+	form.reset();
 };
-
-function onText(event){
-	event.preventDefault();
-	 
-	const formData = new FormData(event.currentTarget);
-	formData.forEach((value, name)=>{
-		mem[name] = value;
-		//throttle((mem) => {
-		localStorage.setItem(LOCAL_KEY, JSON.stringify(mem));
-		//},1000);
-		//console.log(mem);
-})
+//--*function*--//
+function onData(){
+	const mem = {};
+	if(form.email.value){
+			mem.email = form.email.value;
+		}
+	if(form.message.value){
+			mem.message = form.message.value;
+		}
+	localStorage.setItem(LOCAL_KEY, JSON.stringify(mem));
 };
-
-function setText(event){
-	const out = JSON.parse(localStorage.getItem(LOCAL_KEY));
-	console.log('mem');
-	fomrInput.value = out.email;
-	fomrText.value = out.message;
+//--*function*--//
+function setText(){
+	let localStor = JSON.parse(localStorage.getItem(LOCAL_KEY));
+	form.email.value = localStor.email;
+	form.message.value = localStor.message;
 }
+//--*function*--//
+// function onText(event){
+//	const mem = {};
+// 	event.preventDefault();
+// 	const formData = new FormData(event.currentTarget);
+// 	formData.forEach((value, name)=>{
+// 		mem[name] = value;
+// 		localStorage.setItem(LOCAL_KEY, JSON.stringify(mem));
+// })
+// };
